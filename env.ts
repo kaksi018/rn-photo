52f6c61d5e979a2b592fdc37b72ef16fcd35dd31
kaksi018 <jeongyu70@gmail.com>
2024-07-28 13:06:34 +0900

    초기화

---
+++ env.ts
@@ -0,0 +1,11 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCa8VlDJPI-U1Wxa5hND_MYxHrdu9fexz0',
+  authDomain: 'photo-436d2.firebaseapp.com',
+  projectId: 'photo-436d2',
+  storageBucket: 'photo-436d2.appspot.com',
+  messagingSenderId: '282174764214',
+  appId: '1:282174764214:web:8b8f8d983e247901f2a217',
+  measurementId: 'G-L5K7F2QV61',
+};
+
+export const MAP_KEY = {};
